f304a27c8f824a2be5dc46e11c58f11a0f05af44
Andrés Ruiz <andrucuna@Andress-MacBook-Pro.local>
2014-09-10 16:59:57 -0500

    Simple JAX-WS web service
    I will use this as a simple web service to deploy in an application
    server

---
+++ jax-ws/helloservice/src/helloservice/endpoint/Hello.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2009 Sun Microsystems, Inc.
+ * All rights reserved.  You may not modify, use,
+ * reproduce, or distribute this software except in
+ * compliance with  the terms of the License at:
+ * http://developer.sun.com/berkeley_license.html
+ */
+
+
+package helloservice.endpoint;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+
+
+@WebService
+public class Hello {
+    private String message = new String("Hello, ");
+
+    public Hello() {
+    }
+
+    @WebMethod
+    public String sayHello(String name) {
+        return message + name + ".";
+    }
+}
